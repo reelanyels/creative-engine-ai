5a6a76928c62fb3eb70f9ffd57fbd4c3ad068e1e
reelanyels <reelanyels>
2026-04-02 02:25:09 -0600

    Fix: model name to flash-latest

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 # Configurar Gemini
 genai.configure(api_key=os.getenv("AIzaSyB57M1tiZuIJmb2-pnMjNGZ2dT-6VMS9Z0"))
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-1.5-flash-latest')
 
 # --- Interfaz de Streamlit ---
 st.set_page_config(page_title="Creative Engine", layout="centered")
